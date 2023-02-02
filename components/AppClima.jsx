05baa614ef4af1fdf1d1af0881f561971c187e4a
Facundo Veron <facu_ve_2016@hotmail.com>
2023-02-02 02:01:15 -0300

    Comenzando el Proyecto

---
+++ components/AppClima.jsx
@@ -0,0 +1,11 @@
+import Formulario from './Formulario';
+
+export default function AppClima() {
+    return (
+        <>
+            <main className="dos-columnas">
+                <Formulario />
+            </main>
+        </>
+    );
+}
